f5f1ecfcd1326e89b4a1bae29e647e671af3f295
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-02-28 18:06:05 +0100

    fix image url

--- lesson-4/my-app/src/data/travels.js
+++ lesson-4/my-app/src/data/travels.js
@@ -1,7 +1,7 @@
 const travels = [
     {
         id: 1,
-        image: "https://illustrators.ru/uploads/illustration/image/884457/%D0%BF6.jpg",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Panorama_of_Malbork_Castle%2C_part_4.jpg/500px-Panorama_of_Malbork_Castle%2C_part_4.jpg",
         info: {
             title: "замок Мальброк",
             description: "Резиденция Тевтонского ордена",
