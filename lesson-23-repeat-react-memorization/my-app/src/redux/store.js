3ae541ab210e8616deefb6ad49e39c2df3254969
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-14 13:53:43 +0200

    finish lesson 23

--- lesson-23-repeat-react-memorization/my-app/src/redux/store.js
+++ lesson-23-repeat-react-memorization/my-app/src/redux/store.js
@@ -1,8 +1,10 @@
 import { legacy_createStore } from "redux";
 import { devToolsEnhancer } from "@redux-devtools/extension";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./rootReducer";
 
-const store = legacy_createStore(rootReducer, devToolsEnhancer());
+export const store = legacy_createStore(rootReducer, devToolsEnhancer());
 
-export default store;
+export const persistor = persistStore(store);
+
