dece8f87e80a7f0961192a51b80e5a78a1158145
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-14 13:18:23 +0200

    add to cart logic

--- lesson-23-repeat-react-memorization/my-app/src/components/Products/ProductList/ProductList.jsx
+++ lesson-23-repeat-react-memorization/my-app/src/components/Products/ProductList/ProductList.jsx
@@ -1,10 +1,17 @@
+
+
 import ProductListItem from "./ProductListItem/ProductListItem";
 
 import styles from "./ProductList.module.css";
 
 const ProductList = ({ items = [] }) => {
+
+  const onAddProductToCart = ()=> {
+
+  }
+
   const elements = items.map((item) => (
-    <ProductListItem key={item.id} {...item} />
+    <ProductListItem onAddProductToCart={onAddProductToCart} key={item.id} {...item} />
   ));
 
   return <ul className={styles.list}>{elements}</ul>;
--- lesson-23-repeat-react-memorization/my-app/src/components/Products/ProductList/ProductListItem/ProductListItem.jsx
+++ lesson-23-repeat-react-memorization/my-app/src/components/Products/ProductList/ProductListItem/ProductListItem.jsx
@@ -1,13 +1,13 @@
 import styles from "./ProductListItem.module.css";
 
-const ProductListItem = ({ image, name, price }) => {
+const ProductListItem = ({ id, image, name, price, onAddProductToCart }) => {
   return (
     <li>
       <img src={image} className="img-fluid" />
       <p className={styles.name}>{name}</p>
       <div className={styles.priceWrapper}>
         <p className={styles.price}>{price} $</p>
-        <span className={styles.toCart}>To cart</span>
+        <span onClick={()=> onAddProductToCart(id)} className={styles.toCart}>To cart</span>
       </div>
     </li>
   );
