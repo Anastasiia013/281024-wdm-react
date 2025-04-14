618830aa0fb03b4b2231e8652c46fb1a89110ed4
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-14 10:17:19 +0200

    create pages

---
+++ lesson-23-repeat-react-memorization/my-app/src/pages/ProductsPage/ProductsPage.jsx
@@ -0,0 +1,11 @@
+import PageLayout from "../../components/PageLayout/PageLayout";
+
+const ProductsPage = () => {
+  return (
+    <PageLayout title="Products page">
+
+    </PageLayout>
+  );
+};
+
+export default ProductsPage;
