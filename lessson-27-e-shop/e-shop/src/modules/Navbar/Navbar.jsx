1c146ac89319a7b637082cd11e5eaef0678a4be5
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-23 14:43:06 +0200

    add navbar logo

--- lessson-27-e-shop/e-shop/src/modules/Navbar/Navbar.jsx
+++ lessson-27-e-shop/e-shop/src/modules/Navbar/Navbar.jsx
@@ -2,13 +2,15 @@
 
 import Container from "../layouts/Container/Container";
 
+import NavbarLogo from "./NavbarLogo/NavbarLogo";
+
 import { navbarStyle } from "./styles";
 
 const Navbar = ()=> {
     return (
         <nav css={navbarStyle}>
             <Container>
-                
+                <NavbarLogo />
             </Container>
         </nav>
     )
