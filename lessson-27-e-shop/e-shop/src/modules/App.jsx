3eac68860fa860484488e93118f7135fa06d635f
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-23 13:31:15 +0200

    add theme colors

--- lessson-27-e-shop/e-shop/src/modules/App.jsx
+++ lessson-27-e-shop/e-shop/src/modules/App.jsx
@@ -1,11 +1,15 @@
 import { Global } from "@emotion/react";
 
+import Theme from "../context/Theme";
+
 import globalStyles from "../shared/styles/global";
 
 function App() {
   return (
     <Global styles={globalStyles}>
-      
+      <Theme>
+        
+      </Theme>
     </Global>
   )
 }
