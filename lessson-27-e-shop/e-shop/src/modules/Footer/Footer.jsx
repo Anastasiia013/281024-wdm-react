de99768f0481d338a3fd17433ee5f27de9e9b9d6
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-23 15:33:27 +0200

    add footer social icons

--- lessson-27-e-shop/e-shop/src/modules/Footer/Footer.jsx
+++ lessson-27-e-shop/e-shop/src/modules/Footer/Footer.jsx
@@ -3,6 +3,7 @@
 import Container from "../layouts/Container/Container";
 
 import FooterContacts from "./FooterContacts/FooterContacts";
+import FooterSocialLinks from "./FooterSocialLinks/FooterSocialLinks";
 
 import { footerStyle, footerContentStyle } from "./styles";
 
@@ -12,6 +13,7 @@ const Footer = ()=> {
             <Container>
                 <div css={footerContentStyle}>
                     <FooterContacts />
+                    <FooterSocialLinks />
                 </div>
             </Container>
         </footer>
