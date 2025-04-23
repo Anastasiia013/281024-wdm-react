b1158ade5bd35df53e091ec2610636df06825bfe
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-23 14:36:27 +0200

    add Container component

--- lessson-27-e-shop/e-shop/src/modules/Navbar/Navbar.jsx
+++ lessson-27-e-shop/e-shop/src/modules/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 /** @jsxImportSource @emotion/react */
 
+import Container from "../layouts/Container/Container";
+
 import { navbarStyle } from "./styles";
 
 const Navbar = ()=> {
     return (
-        <nav css={navbarStyle}></nav>
+        <nav css={navbarStyle}>
+            <Container>
+                
+            </Container>
+        </nav>
     )
 }
 
