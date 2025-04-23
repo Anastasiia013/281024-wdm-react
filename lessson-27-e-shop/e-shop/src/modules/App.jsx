d08830622523180a9960b197ebf2935fe80b142e
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-23 13:44:31 +0200

    Add Routes

--- lessson-27-e-shop/e-shop/src/modules/App.jsx
+++ lessson-27-e-shop/e-shop/src/modules/App.jsx
@@ -2,16 +2,17 @@ import { Global } from "@emotion/react";
 
 import Theme from "../context/Theme";
 
+import Navigation from "../pages/Navigation";
+
 import globalStyles from "../shared/styles/global";
 
 function App() {
   return (
-    <Global styles={globalStyles}>
-      <Theme>
-        
-      </Theme>
-    </Global>
-  )
+    <Theme>
+      <Global styles={globalStyles} />
+      <Navigation />
+    </Theme>
+  );
 }
 
-export default App
+export default App;
