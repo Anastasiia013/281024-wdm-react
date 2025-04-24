b4a3b62f95a6053fc826b29c27b1ec469475a978
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-24 10:28:52 +0200

    finish product card

--- lesson-28-e-shop-part-2/e-shop/src/pages/MainPage/MainPage.jsx
+++ lesson-28-e-shop-part-2/e-shop/src/pages/MainPage/MainPage.jsx
@@ -3,13 +3,17 @@ import Container from "../../modules/layouts/Container/Container";
 import SectionTitle from "../../shared/components/SectionTitle/SectionTitle";
 import ProductCard from "../../shared/components/ProductCard/ProductCard";
 
-const MainPage = ()=> {
-    return (
-        <Container>
-            <SectionTitle title="Товары" />
-            <ProductCard />
-        </Container>
-    )
-}
+const MainPage = () => {
+  return (
+    <Container>
+      <SectionTitle title="Товары" />
+      <ProductCard
+        img="https://static.nike.com/a/images/t_PDP_936_v1/f_auto%2Cq_auto%3Aeco/awjogtdnqxniqqk0wpgf/AIR%2BMAX%2B270.png"
+        title="Мужские Кроссовки Nike Air Zoom Pegasus"
+        price={180}
+      />
+    </Container>
+  );
+};
 
 export default MainPage;
--- lesson-28-e-shop-part-2/e-shop/src/shared/components/ProductCard/ProductCard.jsx
+++ lesson-28-e-shop-part-2/e-shop/src/shared/components/ProductCard/ProductCard.jsx
@@ -13,23 +13,23 @@ import {
   productCardAddToCartIconStyle,
 } from "./styles";
 
-const ProductCard = () => {
+const ProductCard = ({id, img, title, price, addToCart}) => {
   return (
     <div css={productCardWrapperStyle}>
       <div css={productCardImageWrapperStyle}>
         <img
           css={productCardImageStyle}
-          src="https://static.nike.com/a/images/t_PDP_936_v1/f_auto%2Cq_auto%3Aeco/awjogtdnqxniqqk0wpgf/AIR%2BMAX%2B270.png"
-          alt=""
+          src={img}
+          alt={title}
         />
       </div>
-      <p css={productCardTitleStyle}>Мужские Кроссовки Nike Air Zoom Pegasus</p>
+      <p css={productCardTitleStyle}>{title}</p>
       <div css={productCardActionsStyle}>
         <div>
           <span css={productCardPriceNameStyle}>Цена:</span>
-          <span css={productCardPriceStyle}>180 €</span>
+          <span css={productCardPriceStyle}>{price} €</span>
         </div>
-        <span css={productCardAddToCartIconStyle}><AddToCartIcon /></span>
+        <span css={productCardAddToCartIconStyle} onClick={()=>addToCart(id)}><AddToCartIcon /></span>
       </div>
     </div>
   );
