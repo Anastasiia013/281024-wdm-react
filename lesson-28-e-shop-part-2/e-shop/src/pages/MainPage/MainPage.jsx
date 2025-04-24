f671ae62e99084788e61d21d82b6bdff797b0eed
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-24 09:53:22 +0200

    add product card html

--- lesson-28-e-shop-part-2/e-shop/src/pages/MainPage/MainPage.jsx
+++ lesson-28-e-shop-part-2/e-shop/src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import Container from "../../modules/layouts/Container/Container";
 
 import SectionTitle from "../../shared/components/SectionTitle/SectionTitle";
+import ProductCard from "../../shared/components/ProductCard/ProductCard";
 
 const MainPage = ()=> {
     return (
         <Container>
             <SectionTitle title="Товары" />
+            <ProductCard />
         </Container>
     )
 }
---
+++ lesson-28-e-shop-part-2/e-shop/src/shared/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,21 @@
+/** @jsxImportSource @emotion/react */
+
+const ProductCard = ()=> {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <p>Мужские Кроссовки Nike Air Zoom Pegasus </p>
+            <div>
+                <div>
+                    <span>Цена:</span>
+                    <span>180 €</span>
+                </div>
+                <span>+</span>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard;
