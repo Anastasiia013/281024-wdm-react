bdfbbccebb6c26b8f377ca2ac6647e1aa508536c
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-24 09:30:20 +0200

    add Button component

---
+++ lesson-28-e-shop-part-2/e-shop/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,8 @@
+import Button from "../../shared/components/Button/Button";
+
+const MainPage = ()=> {
+    return (
+    <div style={{padding: "100px"}}><Button>Отправить</Button></div>)
+}
+
+export default MainPage;
