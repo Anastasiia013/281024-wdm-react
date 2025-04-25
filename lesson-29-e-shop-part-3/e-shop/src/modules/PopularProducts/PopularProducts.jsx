e47e62fa6ae5beb8eea74d12216a1ad20265e743
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-25 09:15:36 +0200

    add popular products component

---
+++ lesson-29-e-shop-part-3/e-shop/src/modules/PopularProducts/PopularProducts.jsx
@@ -0,0 +1,14 @@
+import Container from "../layouts/Container/Container";
+
+import SectionTitle from "../../shared/components/SectionTitle/SectionTitle";
+import ProductCard from "../../shared/components/ProductCard/ProductCard";
+
+const PopularProducts = ()=> {
+    return (
+        <Container>
+            <SectionTitle title="Товары" />
+        </Container>
+    )
+}
+
+export default PopularProducts;
