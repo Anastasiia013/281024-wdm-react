b7e8b863b689c265f9706a6bb01b5959b936ce30
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-25 09:44:30 +0200

    add decorator example

--- lesson-29-e-shop-part-3/e-shop/src/modules/PopularProducts/PopularProducts.jsx
+++ lesson-29-e-shop-part-3/e-shop/src/modules/PopularProducts/PopularProducts.jsx
@@ -1,14 +1,36 @@
+import { useState, useEffect } from "react";
+
 import Container from "../layouts/Container/Container";
 
 import SectionTitle from "../../shared/components/SectionTitle/SectionTitle";
 import ProductCard from "../../shared/components/ProductCard/ProductCard";
 
-const PopularProducts = ()=> {
-    return (
-        <Container>
-            <SectionTitle title="Товары" />
-        </Container>
-    )
-}
+import { getPopularProducts } from "../../shared/api/products-api";
+
+const PopularProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPopularProducts = async () => {
+      setLoading(true);
+      const { data, error } = await getPopularProducts();
+      setLoading(false);
+      if (error) {
+        return setError(error.response.data.message);
+      }
+      setProducts(data);
+    };
+
+    fetchPopularProducts();
+  }, []);
+
+  return (
+    <Container>
+      <SectionTitle title="Товары" />
+    </Container>
+  );
+};
 
 export default PopularProducts;
--- lesson-29-e-shop-part-3/e-shop/src/shared/api/products-api.js
+++ lesson-29-e-shop-part-3/e-shop/src/shared/api/products-api.js
@@ -1,6 +1,13 @@
 import backendInstance from "./backendInstance";
 
-export const getPopularProducts = async ()=> {
-    const {data} = await backendInstance.get("/products");
+import requestDecorator from "./requestDecorator";
+
+export const getPopularProducts = requestDecorator(async () => {
+    const { data } = await backendInstance.get("/products");
     return data.data;
-}
+})
+
+export const getProductById = requestDecorator(async (id) => {
+    const { data } = await backendInstance.get(`/products/${id}`);
+    return data.data;
+})
