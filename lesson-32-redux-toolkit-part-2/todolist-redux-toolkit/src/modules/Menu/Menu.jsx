27f910fa4f2608e66512851d1243ca3dfe3f0856
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-30 09:39:53 +0200

    add menu

--- lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/Menu/Menu.jsx
+++ lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/Menu/Menu.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
-import { Nav } from "rsuite";
+import { FlexboxGrid, Nav } from "rsuite";
+import { Link } from "react-router-dom";
+
+import styles from "./Menu.module.css";
 
 const Menu = () => {
   const [active, setActive] = useState("home");
 
   return (
-    <Nav appearance="pills" activeKey={active} onSelect={setActive}>
-      <Nav.Item eventKey="home">To Do</Nav.Item>
-      <Nav.Item eventKey="completed">To Do Completed</Nav.Item>
-    </Nav>
+    <FlexboxGrid justify="center">
+      <Nav appearance="pills" activeKey={active} onSelect={setActive}>
+        <Link to="/" className={styles.link}>
+          <Nav.Item as="span" eventKey="home">
+            To Do
+          </Nav.Item>
+        </Link>
+        <Link to="/completed" className={styles.link}>
+          <Nav.Item as="span" eventKey="completed">
+            To Do Completed
+          </Nav.Item>
+        </Link>
+      </Nav>
+    </FlexboxGrid>
   );
 };
 
