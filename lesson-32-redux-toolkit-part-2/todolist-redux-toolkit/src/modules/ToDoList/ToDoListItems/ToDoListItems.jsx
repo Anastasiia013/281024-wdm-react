6bb877e0d1a21928159e1e52f16a7733dad60558
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-30 12:02:43 +0200

    add singletodoitem

--- lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoListItems/ToDoListItems.jsx
+++ lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoListItems/ToDoListItems.jsx
@@ -1,12 +1,12 @@
 import { useDispatch } from "react-redux";
-import { List, Button, FlexboxGrid } from "rsuite";
+import { List } from "rsuite";
+
+import SingleTodoItem from "./SingleTodoItem/SingleTodoItem";
 
 import {
   deleteTodo,
   toggleTodoCompleted,
 } from "../../../redux/todo/todo-slice";
-
-import styles from "./ToDoListItems.module.css";
 
 const ToDoListItems = ({ items = [] }) => {
   const dispatch = useDispatch();
@@ -19,35 +19,13 @@ const ToDoListItems = ({ items = [] }) => {
     dispatch(toggleTodoCompleted(id));
   };
 
-  const elements = items.map(({ id, todo, completed }) => (
-    <List.Item key={id}>
-      <FlexboxGrid justify="space-between" align="middle">
-        <FlexboxGrid.Item
-          colspan={15}
-          className={completed ? styles.completed : ""}
-        >
-          {todo}
-        </FlexboxGrid.Item>
-
-        <FlexboxGrid.Item colspan={9}>
-          <FlexboxGrid justify="end" className={styles.actions}>
-            <Button
-              onClick={() => onToggleTodoCompleted(id)}
-              appearance="primary"
-            >
-              {completed ? "Undo" : "Completed"}
-            </Button>
-            <Button
-              onClick={() => onDeleteTodo(id)}
-              appearance="primary"
-              color="red"
-            >
-              Delete
-            </Button>
-          </FlexboxGrid>
-        </FlexboxGrid.Item>
-      </FlexboxGrid>
-    </List.Item>
+  const elements = items.map((item) => (
+    <SingleTodoItem
+      key={item.id}
+      {...item}
+      onDeleteTodo={onDeleteTodo}
+      onToggleTodoCompleted={onToggleTodoCompleted}
+    />
   ));
 
   return <List bordered>{elements}</List>;
