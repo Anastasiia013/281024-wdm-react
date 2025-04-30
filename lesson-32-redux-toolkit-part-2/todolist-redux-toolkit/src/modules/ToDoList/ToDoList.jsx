b2e0308948a9329046f39d38aa8aa8e718255e42
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-30 11:12:58 +0200

    add list UI

--- lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoList.jsx
+++ lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoList.jsx
@@ -1,12 +1,27 @@
 import ToDoListForm from "./ToDoListForm/ToDoListForm";
 import ToDoListItems from "./ToDoListItems/ToDoListItems";
 
+import styles from "./ToDoList.module.css";
+
+const mockToDoItems = [
+    {
+        id: "23tdga",
+        todo: "Покормить кота",
+        completed: false,
+    },
+    {
+        id: "23tdg1",
+        todo: "Покормить кота",
+        completed: true,
+    },
+]
+
 const ToDoList = () => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <ToDoListForm />
-      <ToDoListItems />
-    </>
+      <ToDoListItems items={mockToDoItems} />
+    </div>
   );
 };
 
--- lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoListItems/ToDoListItems.jsx
+++ lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoListItems/ToDoListItems.jsx
@@ -1,5 +1,31 @@
-const ToDoListItems = ()=> {
+import { List, Button, FlexboxGrid } from "rsuite";
 
-}
+import styles from "./ToDoListItems.module.css";
+
+const ToDoListItems = ({ items = [] }) => {
+  const elements = items.map(({ id, todo, completed }) => (
+    <List.Item key={id}>
+      <FlexboxGrid justify="space-between" align="middle">
+        <FlexboxGrid.Item
+          colspan={15}
+          className={completed ? styles.completed : ""}
+        >
+          {todo}
+        </FlexboxGrid.Item>
+
+        <FlexboxGrid.Item colspan={9} className={styles.actions}>
+          <Button appearance="primary">
+            {completed ? "Undo" : "Completed"}
+          </Button>
+          <Button appearance="primary" color="red">
+            Delete
+          </Button>
+        </FlexboxGrid.Item>
+      </FlexboxGrid>
+    </List.Item>
+  ));
+
+  return <List bordered>{elements}</List>;
+};
 
 export default ToDoListItems;
