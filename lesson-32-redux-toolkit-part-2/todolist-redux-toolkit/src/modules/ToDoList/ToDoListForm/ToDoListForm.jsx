beebf4321eeec6271d673ef14f84b420806888b1
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-30 10:10:56 +0200

    add todo form label+input

--- lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoListForm/ToDoListForm.jsx
+++ lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoListForm/ToDoListForm.jsx
@@ -1,5 +1,36 @@
-const ToDoListForm = ()=> {
+import { useId } from "react";
+import { useForm } from "react-hook-form";
+import { Form, Input, Button } from "rsuite";
 
-}
+const ToDoListForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values);
+    reset();
+  };
+
+  const todoId = useId();
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group>
+        <Form.ControlLabel id={todoId}>New todo</Form.ControlLabel>
+        <Input
+          {...register("todo", {
+            required: "Todo must be exist",
+          })}
+          placeholder="Enter new todo"
+          id={todoId}
+        />
+      </Form.Group>
+    </Form>
+  );
+};
 
 export default ToDoListForm;
