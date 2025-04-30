bea22ce6c16618731d1ddd0fdcda71259ae95967
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-04-30 11:28:14 +0200

    add selectAllTodo

--- lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoList.jsx
+++ lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/modules/ToDoList/ToDoList.jsx
@@ -1,13 +1,19 @@
+import { useSelector } from "react-redux";
+
 import ToDoListForm from "./ToDoListForm/ToDoListForm";
 import ToDoListItems from "./ToDoListItems/ToDoListItems";
+
+import { selectAllTodo } from "../../redux/todo/todo-selectors";
 
 import styles from "./ToDoList.module.css";
 
 const ToDoList = () => {
+  const items = useSelector(selectAllTodo);
+
   return (
     <div className={styles.wrapper}>
       <ToDoListForm />
-      <ToDoListItems items={[]} />
+      <ToDoListItems items={items} />
     </div>
   );
 };
---
+++ lesson-32-redux-toolkit-part-2/todolist-redux-toolkit/src/redux/todo/todo-selectors.js
@@ -0,0 +1,1 @@
+export const selectAllTodo = store => store.todo;
