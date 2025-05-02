329e2a863dd839364f2ca8dda4dbec50c225f5ff
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-05-02 13:18:03 +0200

    add form

--- lesson-33-redux-toolkit-repeat/my-movies/src/modules/MyMovies/MovieAddForm/MovieAddForm.jsx
+++ lesson-33-redux-toolkit-repeat/my-movies/src/modules/MyMovies/MovieAddForm/MovieAddForm.jsx
@@ -1,7 +1,63 @@
 import { useForm } from "react-hook-form";
 
-const MovieAddForm = ()=> {
+import styles from "./MovieAddForm.module.css";
 
-}
+const MovieAddForm = (submitForm) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (values) => {
+    submitForm(values);
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.block}>
+        <label>Movie title:</label>
+        <input
+          {...register("title", { required: "Title required" })}
+          placeholder="Movie title"
+        />
+        {errors.title && <p className={styles.error}>{errors.title.message}</p>}
+      </div>
+      <div className={styles.block}>
+        <label>Movie director:</label>
+        <input
+          {...register("director")}
+          placeholder="Movie Director"
+        />
+      </div>
+      <div className={styles.block}>
+        <label>Movie release year:</label>
+        <input
+          {...register("releaseYear")}
+          placeholder="Movie release year"
+        />
+      </div>
+      <div className={styles.block}>
+        <label>Movie favorite:</label>
+        <input
+          {...register("favorite")}
+          type="checkbox"
+        />
+      </div>
+      <div className={styles.block}>
+        <label>Movie genre:</label>
+        <select
+          {...register("genre")}>
+            <option value="film">Film</option>
+            <option value="serial">Serial</option>
+            <option value="cartoon">Cartoon</option>
+            </select>
+      </div>
+      <button type="submit">Add movie</button>
+    </form>
+  );
+};
 
 export default MovieAddForm;
