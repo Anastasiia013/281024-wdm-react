6e01613b7d4967857b48d0ca98f125abb286c683
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-05-07 10:27:10 +0200

    add Container & PageTitle components

---
+++ lesson-36-register-login/my-app/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import Container from "../../shared/components/Container/Container";
+import PageTitle from "../../shared/components/PageTitle/PageTitle";
+
+const HomePage = ()=> {
+    return (
+        <Container>
+            <PageTitle>Home page</PageTitle>
+        </Container>
+    )
+}
+
+export default HomePage;
