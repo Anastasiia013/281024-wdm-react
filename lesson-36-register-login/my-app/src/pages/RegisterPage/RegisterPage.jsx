389a0cfa4ecfa1a68a306b70b906b606f5ff066e
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-05-07 11:33:34 +0200

    add register form

--- lesson-36-register-login/my-app/src/pages/RegisterPage/RegisterPage.jsx
+++ lesson-36-register-login/my-app/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,13 @@
 import Container from "../../shared/components/Container/Container";
 import PageTitle from "../../shared/components/PageTitle/PageTitle";
 
+import Register from "../../modules/Register/Register";
+
 const RegisterPage = ()=> {
     return (
         <Container>
             <PageTitle>Register</PageTitle>
+            <Register />
         </Container>
     )
 }
